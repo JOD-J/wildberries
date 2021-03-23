8f9584232d4cb4f859a36750a7ff72fe08288e51
Евгений Бахуринский <54557008+JOD-J@users.noreply.github.com>
2021-03-23 21:13:59 +0500

    add lesson-01

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+'use strict';
 const mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 
@@ -6,5 +7,37 @@ const mySwiper = new Swiper('.swiper-container', {
 		nextEl: '.slider-button-next',
 		prevEl: '.slider-button-prev',
 	},
+});
+
+// cart
+const buttonCart = document.querySelector('.button-cart'),
+modalCart = document.querySelector('#modal-cart'),
+modalClose = document.querySelector('.modal-close');
+const showCart = () => {
+	modalCart.classList.add('show')
+};
+const closeCart = () => {
+	modalCart.classList.remove('show')
+};
+buttonCart.addEventListener('click', showCart);
+modalCart.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target === modalClose || target === modalCart) {
+		closeCart();
+	}
+});
+
+
+// scroll-link
+const scrollLinks = document.querySelectorAll('a.scroll-link');
+scrollLinks.forEach((item, index) => {
+	item.addEventListener('click', (e) => {
+		e.preventDefault();
+		const idScroll = item.getAttribute('href')
+		document.querySelector(idScroll).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
 });
 
