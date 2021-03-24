58d52985150304e1b2e67a76a8da18b9ccea35db
Евгений Бахуринский <54557008+JOD-J@users.noreply.github.com>
2021-03-25 00:05:01 +0500

    add lesson-02

--- js/main.js
+++ js/main.js
@@ -28,16 +28,90 @@ modalCart.addEventListener('click', (e) => {
 });
 
 
+// общий скрол
+const scrollTop = (elem) => {
+	const idScroll = elem.getAttribute('href')
+	document.querySelector(idScroll).scrollIntoView({
+		behavior: 'smooth',
+		block: 'start',
+	})
+};
+
 // scroll-link
 const scrollLinks = document.querySelectorAll('a.scroll-link');
 scrollLinks.forEach((item, index) => {
 	item.addEventListener('click', (e) => {
 		e.preventDefault();
-		const idScroll = item.getAttribute('href')
-		document.querySelector(idScroll).scrollIntoView({
-			behavior: 'smooth',
-			block: 'start',
-		})
+		scrollTop(item);
 	})
 });
 
+
+// goods
+const more = document.querySelector('.more');
+const navigationLinks = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async () => {
+	const result = await fetch('db/db.json')
+	if (!result.ok) {
+		throw 'Ошбка вышла:' + result.status;
+	}
+	return result.json();
+	
+};
+
+const createCard = ({category, description, gender, id, img, label, name, price }) => {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'
+	card.innerHTML = `
+			<div class="goods-card">
+			${label ?
+				`<span class="label">${label}</span>`:
+				''}
+			<img src="db/${img}" alt="${name}" class="goods-image">
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
+				<span class="button-price">$${price}</span>
+			</button>
+			</div>
+	`
+	return card;
+};
+
+const renderCards = (data) => {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods')
+};
+
+more.addEventListener('click', (event) => {
+	event.preventDefault();
+	getGoods().then(renderCards)
+	scrollTop(more);
+});
+
+const filterCards = (field, value) => {
+	getGoods().then((data) => {
+		const filterdGoods = data.filter((good) =>{
+			return good[field] === value			
+		});
+		if (filterdGoods.length) {
+			return filterdGoods;
+		} else {
+			return data;
+		}
+	})
+	.then(renderCards);
+};
+
+navigationLinks.forEach((linlk) => {
+	linlk.addEventListener('click', (event) => {
+		event.preventDefault();
+		const field = linlk.dataset.field;
+		const value = linlk.textContent;
+		filterCards(field, value);
+	});
+});
